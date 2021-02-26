5091a540f9532d37c9c41364a0eb9cd39eaee5ba
MichMatch <michael@graphicdesire.se>
2021-02-26 10:28:30 +0100

    Category entity

---
+++ src/main/java/Tankesmedjan/TSWebShop/entity/CategoryEntity.java
@@ -0,0 +1,50 @@
+package Tankesmedjan.TSWebShop.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Categories")
+public class CategoryEntity {
+
+    private String name;
+    private String shortname;
+    private int id;
+    private int parentid;
+
+    public CategoryEntity(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortname() {
+        return shortname;
+    }
+
+    public void setShortname(String shortname) {
+        this.shortname = shortname;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getParentid() {
+        return parentid;
+    }
+
+    public void setParentid(int parentid) {
+        this.parentid = parentid;
+    }
+}
+
