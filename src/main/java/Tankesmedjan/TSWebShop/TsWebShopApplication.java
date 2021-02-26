d02f164e61151eed4bd943bc980db4448ce09854
MichMatch <michael@graphicdesire.se>
2021-02-26 11:49:24 +0100

    Encryption of application.properties

--- src/main/java/Tankesmedjan/TSWebShop/TsWebShopApplication.java
+++ src/main/java/Tankesmedjan/TSWebShop/TsWebShopApplication.java
@@ -1,8 +1,12 @@
 package Tankesmedjan.TSWebShop;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableEncryptableProperties
 @SpringBootApplication
 public class TsWebShopApplication {
 
