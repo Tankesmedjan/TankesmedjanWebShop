b1fe03b3037c5329331abf08e269d379fdd0378f
MichMatch <michael@graphicdesire.se>
2021-02-26 09:00:53 +0100

    first commit

---
+++ src/main/java/Tankesmedjan/TSWebShop/TsWebShopApplication.java
@@ -0,0 +1,13 @@
+package Tankesmedjan.TSWebShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsWebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsWebShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tankesmedjan/TSWebShop/TsWebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Tankesmedjan.TSWebShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsWebShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
