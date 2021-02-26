a880a1b289b667b969cc6559a2e344912e567c22
MichMatch <michael@graphicdesire.se>
2021-02-26 10:35:17 +0100

    Category service

---
+++ src/main/java/Tankesmedjan/TSWebShop/service/CategoryService.java
@@ -0,0 +1,18 @@
+package Tankesmedjan.TSWebShop.service;
+
+import Tankesmedjan.TSWebShop.entity.CategoryEntity;
+import Tankesmedjan.TSWebShop.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@Transactional
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+    public List<CategoryEntity> listAllUser() {return categoryRepository.findAll();}
+}
